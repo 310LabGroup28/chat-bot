80cbcd18bdf1881dd111a8519553e0128dd6836c
colevs <colervs@gmail.com>
2021-03-06 17:31:10 -0800

    Cleaned up code a bit.

--- chat-bot/location.py
+++ chat-bot/location.py
@@ -4,7 +4,6 @@ import geopy.distance
 
 class Location(object):
 
-
 	def getLocation(place = None):
 		if place is None:
 			client = IpregistryClient("fs9pbuwmnx5r2g")  
@@ -17,14 +16,12 @@ class Location(object):
 			location = geolocator.geocode(place)
 			return (location.latitude, location.longitude)
 
-	def distanceByLatLong(lat, long):
-		coord1 = getLocation()
-		coord2 = (lat, long)
-		return geopy.distance.distance(coord1, coord2).km
+	def distanceByLatLong(lat1, long1, lat2 = None, long2 = None):
+		if lat2 is None:
+			coord1 = (lat1, long1)
+			coord2 = getLocation()
+		else:
+			coord1 = (lat1, long1)
+			coord2 = (lat2, long2)
 
-	def distanceByLatLong(lat1, long1, lat2, long2):
-		coord1 = (lat1, long1)
-		coord2 = (lat2, long2)
 		return geopy.distance.distance(coord1, coord2).km
-
-	
