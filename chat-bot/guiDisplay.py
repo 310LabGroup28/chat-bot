7ecd835ec33eeb2c2ff99c9c007723985f401e63
colevs <colervs@gmail.com>
2021-03-11 23:34:20 -0800

    Changed display text on GUI.

--- chat-bot/guiDisplay.py
+++ chat-bot/guiDisplay.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append(sys.path[0]+"\lib")
+sys.path.append(sys.path[0]+"\\lib")
 import tkinter as tk
 from responsechatbot import ChatbotResponse as cr
 
@@ -22,16 +22,16 @@ master = tk.Tk()
 master.geometry("1000x400")
 
 
-tk.Label(master, text="Question: ").grid(row=0, column=0)
+tk.Label(master, text="Say something to Chatbot below then press 'Send'").grid(row=0, column=0)
 
 e1 = tk.Entry(master, width=160)
 e1.grid(row=1, column=0, padx=5, pady=5)
 
-send = tk.Button(master, text='Show', command=insertContent)
+send = tk.Button(master, text='Send', command=insertContent)
 send.grid(row=3, column=0, sticky=tk.W, pady=5)
 
 
-record = "========hello, please enter your questions========"
+record = ""
 T = tk.Text(master, height=20, width=140)
 T.grid(row=5, column=0, sticky=tk.W, pady=4, padx=4)
 T.tag_configure("even", background="#ffffff")
