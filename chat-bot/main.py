1acfb29023ef920f22377d9f476def708436adb5
unknown <mansonyu0728@gmail.com>
2021-03-24 16:48:20 -0700

    added posTag_spellCheck class, also modified mian.py for postag and spellcheck to work

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-import nltk
 from responsechatbot import ChatbotResponse as cr
-from outofscoperesponse import OutOfScope as oos
+from posTag_spellCheck import check, pos_tagging
 
-nltk.download('wordnet')
 
 def show_entry_fields():
     print("First Name: %s" % (e1.get()))
@@ -11,17 +9,41 @@ def show_entry_fields():
 
 def insertContent():
     reply = ""
-
+    #    
     try:
         reply = cr.getResponse(e1.get())
     except:
-        reply = oos.getResponse()
+        reply = "I'm sorry, I didn't quite understand that.\nTry asking for help to see the scope of my functionality, or try asking another question.\n"
 
     print(reply)
     T.insert(tk.END, e1.get() + '\n', "odd")
     T.insert(tk.END, reply + '\n', "even")
     e1.delete(0, tk.END)
-    
+
+def checkSpelling():
+    sentence = e1.get()
+    #print(pos_tagging(sentence))
+    dic = check(sentence)
+    tips = 'here are some spelling mistakes:\n'
+    for k in dic:
+        tips = tips + str(k) + ' -> ' + str(dic[k]) + '\n'
+    if len(dic) == 0:
+        tips = 'it seems all spelling are correct\n'    
+    T.insert(tk.END, tips + '\n', "tip")
+    print(tips)
+    return
+
+def posTagging():
+    sentence = e1.get()
+    dic = pos_tagging(sentence)
+    tips = 'here are the POS tagging:\n'
+    for k in dic:
+        tips = tips + str(k) + ' -> ' + str(dic[k]) + '\n'
+    if len(dic) == 0:
+        tips = 'it is empty\n'    
+    T.insert(tk.END, tips + '\n', "tip")
+    print(tips)
+    return 
 
 master = tk.Tk()
 master.geometry("1000x400")
@@ -35,12 +57,18 @@ e1.grid(row=1, column=0, padx=5, pady=5)
 send = tk.Button(master, text='Send', command=insertContent)
 send.grid(row=3, column=0, sticky=tk.W, pady=5)
 
+checkSpell = tk.Button(master, text='check spelling', command=checkSpelling)
+checkSpell.grid(row=4, column=0, sticky=tk.W, pady=5)
+
+posTag = tk.Button(master, text='POS tagging', command=posTagging)
+posTag.grid(row=5, column=0, sticky=tk.W, pady=5)
 
 record = "==========================================================Chat Log==========================================================\n"
 T = tk.Text(master, height=20, width=140)
-T.grid(row=5, column=0, sticky=tk.W, pady=4, padx=4)
+T.grid(row=6, column=0, sticky=tk.W, pady=4, padx=4)
 T.tag_configure("even", background="#ffffff")
 T.tag_configure("odd", background="#7bbfea")
+T.tag_configure("tip", background="#ccffff")
 T.insert(tk.END, record)
 
 
