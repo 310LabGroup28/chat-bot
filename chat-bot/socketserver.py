f116a46cf41b0d54faed2eac7ffc9e081da14ea8
colevs <colervs@gmail.com>
2021-03-27 14:25:05 -0700

    Added comments to socketserver.py

--- chat-bot/socketserver.py
+++ chat-bot/socketserver.py
@@ -4,19 +4,26 @@ from responsechatbot import ChatbotResponse as cr
 IP = 'PLACEHOLDER_IP'
 PORT = 4000
 
+# Open a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind socket to local IP
 s.bind((IP, PORT))
 s.listen(5)
+# Accept connection from client
 clientsocket, address = s.accept()
 
+# Starting message since both bots expect the user to send the first input
 msgSend = "Hi Vin, big fan!\n"
+# Stop after 15 turns each
 for i in range(15):
-
+	# Send current message
 	clientsocket.send(bytes(msgSend, "utf-8"))
 	print("Chatbot: " + msgSend)
 
+	# Receive a response
 	msgRec = clientsocket.recv(1024)
 	msgRec = msgRec.decode("utf-8")
 	print("Vinbot: " + msgRec+"\n")
 
+	# Get the next thing to send
 	msgSend = cr.getResponse(msgRec)
